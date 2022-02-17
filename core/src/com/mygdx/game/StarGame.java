3736f99979248ee713e65e271494e82d64200f6e
Dmitriy <dsemenov@saec.ru>
2022-02-17 09:49:49 +0500

    Добавление картинки для фона. Выполнение ДЗ №1

--- core/src/com/mygdx/game/StarGame.java
+++ core/src/com/mygdx/game/StarGame.java
@@ -1,31 +1,38 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.sun.tools.doclint.Entity;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture background;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		background = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.draw(img, 0, 0);
 		batch.end();
+
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		background.dispose();
 	}
 }
